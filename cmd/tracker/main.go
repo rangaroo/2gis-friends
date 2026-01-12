3b8d0c57c64f6f831684f5d7579bf5cdf2d28a80
Aslan Onalbek <71209484+rangaroo@users.noreply.github.com>
2026-01-12 17:38:44 +0500

    Add TUI (#3)
    * Change env path to '.env'
    
    * Add global state with mutexes
    
    * Initial TUI
    
    * Rewrite main.go to include TUI logic
    
    * Display friends data as a table

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"log"
 	"context"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
+
 	"github.com/rangaroo/2gis-friends/internal/client"
 	"github.com/rangaroo/2gis-friends/internal/config"
 	"github.com/rangaroo/2gis-friends/internal/database"
 	"github.com/rangaroo/2gis-friends/internal/handler"
+	"github.com/rangaroo/2gis-friends/internal/ui"
+	"github.com/rangaroo/2gis-friends/internal/state"
 )
 
 func main() {
-	log.Println("Starting 2GIS Friend Tracker...")
-
 	// load config
 	cfg, err := config.Load()
 	if err != nil {
@@ -37,20 +39,35 @@ func main() {
 	// create user cache to store 2GIS friend's profiles
 	userCache := config.NewUserCache()
 
+	// initialize UI state
+	store := state.NewStore()
+
 	// initialize handler
-	handler := handler.New(db, userCache)
+	h := handler.New(db, userCache, store)
 
 	// create context that cancels when Ctrl+C is pressed
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
 	// start supervisor loop that cancels on Ctrl+C and reconnects to websocket if connection is lost
-	supervisorLoop(ctx, cfg, db, handler)
+	go supervisorLoop(ctx, cfg, h, store)
 
-	log.Println("Exiting...")
+	// start UI
+	p := tea.NewProgram(ui.NewModel(store))
+	if _, err := p.Run(); err != nil {
+		log.Printf("Error starting UI: %v", err)
+		os.Exit(1)
+	}
+
+	//log.Println("Exiting...")
 }
 
-func supervisorLoop(ctx context.Context, cfg *config.Config, db *database.Client, handler *handler.Handler) {
+func supervisorLoop(
+	ctx context.Context,
+	cfg *config.Config,
+	h *handler.Handler,
+	store *state.GlobalStore,
+) {
 	timeout := 1 * time.Second
 
 	for {
@@ -59,27 +76,32 @@ func supervisorLoop(ctx context.Context, cfg *config.Config, db *database.Client
 			return
 		}
 
-		err := runTracker(ctx, cfg, db, handler.HandleMessage)
+		_ = runTracker(ctx, cfg, h.HandleMessage, store)
 
 		if ctx.Err() != nil {
 			return
 		}
 
-		log.Printf("Connection lost: %v. Retrying in %v...", err, timeout)
+		// log.Printf("Connection lost: %v. Retrying in %v...", err, timeout)
 
 		select {
 		case <-ctx.Done():
 			return
 		case <-time.After(timeout):
 			timeout *= 2
-			if timeout > 10 * time.Second {
+			if timeout > 10*time.Second {
 				timeout = 10 * time.Second
 			}
 		}
 	}
 }
 
-func runTracker(ctx context.Context, cfg *config.Config, db *database.Client, handleMessage func([]byte)) error {
+func runTracker(
+	ctx context.Context,
+	cfg *config.Config,
+	handleMessage func([]byte),
+	store *state.GlobalStore,
+) error {
 	// connect to client
 	ws, err := client.Connect(cfg)
 	if err != nil {
---
+++ internal/ui/model.go
@@ -0,0 +1,104 @@
+package ui
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
+	"github.com/rangaroo/2gis-friends/internal/state"
+)
+
+type Model struct {
+	store *state.GlobalStore
+	table table.Model
+}
+
+func NewModel(store *state.GlobalStore) Model {
+	columns := []table.Column{
+		{Title: "Name", Width: 15},
+		{Title: "Battery", Width: 10},
+		{Title: "Last updated", Width: 15},
+		{Title: "Coordinates", Width: 20},
+	}
+
+	rows := []table.Row{table.Row{"Waiting..."}}
+
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(15),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	return Model{
+		store: store,
+		table: t,
+	}
+}
+
+func (m Model) Init() tea.Cmd {
+	return tea.Tick(time.Second, func(t time.Time) tea.Msg {
+		return t
+	})
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "q" || msg.String() == "ctrl+c" {
+			return m, tea.Quit
+		}
+	case time.Time:
+		friends := m.store.GetViewData()
+		rows := []table.Row{}
+		for _, f := range friends {
+			ago := time.Since(f.LastSeen).Round(time.Second)
+
+			rows = append(rows, table.Row{
+				f.Name,
+				fmt.Sprintf("%.0f%%", f.Battery),
+				fmt.Sprintf("%s", ago),
+				fmt.Sprintf("%.4f, %.4f", f.Lat, f.Lon),
+			})
+		}
+		m.table.SetRows(rows)
+
+		return m, tea.Tick(time.Second, func (t time.Time) tea.Msg {
+			return t
+		})
+	}
+
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+func (m Model) View() string {
+	s := "\n  📡 2GIS FRIEND TRACKER\n"
+	s += "  ──────────────────────\n"
+
+
+	s += baseStyle.Render(m.table.View()) + "\n"
+
+	s += "\n (Press 'q' to quit)\n"
+	return s
+}
